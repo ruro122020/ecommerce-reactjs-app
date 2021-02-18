975fe29498995d4d76c63d0de4f58be5332b0e62
ruro122020 <ruro122020@gmail.com>
2021-02-18 14:51:20 -0700

    display on small screen

--- src/components/cakes/Cakes.js
+++ src/components/cakes/Cakes.js
@@ -1,9 +1,104 @@
 import React from "react";
+import cakeImg from "../../assets/images/cake.jpg";
+import "./mobile/Cakes.css";
+
+const cakes = [
+  {
+    img: "",
+    name: "",
+    price: "",
+    decription: "",
+    inStock: "",
+  },
+  {
+    img: "",
+    name: "",
+    price: "",
+    decription: "",
+    inStock: "",
+  },
+  {
+    img: "",
+    name: "",
+    price: "",
+    decription: "",
+    inStock: "",
+  },
+  {
+    img: "",
+    name: "",
+    price: "",
+    decription: "",
+    inStock: "",
+  },
+  {
+    img: "",
+    name: "",
+    price: "",
+    decription: "",
+    inStock: "",
+  },
+  {
+    img: "",
+    name: "",
+    price: "",
+    decription: "",
+    inStock: "",
+  },
+  {
+    img: "",
+    name: "",
+    price: "",
+    decription: "",
+    inStock: "",
+  },
+  {
+    img: "",
+    name: "",
+    price: "",
+    decription: "",
+    inStock: "",
+  },
+  {
+    img: "",
+    name: "",
+    price: "",
+    decription: "",
+    inStock: "",
+  },
+  {
+    img: "",
+    name: "",
+    price: "",
+    decription: "",
+    inStock: "",
+  },
+];
 
 const Cakes = () => {
   return (
-    <div>
-      <p>Cakes</p>
+    <div className="cakes_wrapper">
+      {cakes.map((cake) => {
+        return (
+          <div className="cake">
+            <div className="wrapper">
+              <img className="img" src={cakeImg} />
+            </div>
+            <div className="wrapper">
+              <span>name</span>
+            </div>
+            <div className="wrapper">
+              <span>decription</span>
+            </div>
+            <div className="wrapper">
+              <span>price</span>
+            </div>
+            <div className="wrapper">
+              <span>in stock</span>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
